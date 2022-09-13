af9e682177d928000c964c7148893c261c69aa75
kevinsantana <kevin_santana.araujo@hotmail.com>
2022-09-13 11:59:59 -0300

    argparser and main file

--- solutions/exact_solution/backtracking.py
+++ solutions/exact_solution/backtracking.py
@@ -1,9 +1,6 @@
 import math
-import time
 
 from helpers import cliques_from_list, is_solution
-from solutions.heuristics.ant_colony_optimization import read_graph
-from test_instances import test_graph1, test_graph2, test_graph3
 
 
 def backtrack(adj_mat, cliques, v=1, best=(math.inf, None)):
@@ -21,25 +18,3 @@ def backtrack(adj_mat, cliques, v=1, best=(math.inf, None)):
                     best = backtrack(adj_mat, cliques, v + 1, best)
             cliques[v] = 0
     return best
-
-
-def main():
-    # test_graph = read_graph(
-    #     "/media/rebellion/LORDS_BLADE/python/mestrado/aed/ppca-aed-projeto-final/dimacs_benchmark_set/abhik1505040_max_clique_implementations/C125.9.clq",
-    #     backtrack=True,
-    # )
-    # test_graph1 = read_graph(
-    #     "/media/rebellion/LORDS_BLADE/python/mestrado/aed/ppca-aed-projeto-final/dimacs_benchmark_set/abhik1505040_max_clique_implementations/anna.col",
-    #     backtrack=True,
-    # )
-    test_graph = test_graph3
-    start_time = time.time()
-    cliques = [0 for x in range(test_graph.shape[0])]
-    cliques[0] = 1
-    solution = backtrack(test_graph, cliques, 1)
-    print(solution[0], "cliques:", solution[1])
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-
-if __name__ == "__main__":
-    main()
---
+++ solutions/main.py
@@ -0,0 +1,173 @@
+import os
+import glob
+import _thread
+import argparse
+import threading
+from contextlib import contextmanager
+
+import numpy as np
+import pandas as pd
+from loguru import logger
+
+from solutions.heuristics.ant_colony_optimization import AntClique, read_graph
+
+
+class TimeoutException(Exception):
+    pass
+
+
+@contextmanager
+def time_limit(seconds):
+    timer = threading.Timer(seconds, lambda: _thread.interrupt_main())
+    timer.start()
+    try:
+        yield
+    except KeyboardInterrupt:
+        raise TimeoutException()
+    finally:
+        timer.cancel()
+
+
+def parse_input_args():
+    root_parser = argparse.ArgumentParser(add_help=False)
+
+    root_parser.add_argument(
+        "--input-dir",
+        dest="input_dir",
+        help="Run the program on all files inside dimacs_benchmark_set",
+    )
+
+    root_parser.add_argument(
+        "-i",
+        "--input-path",
+        dest="input_path",
+        help="Input path for single file",
+        default=os.path.join(
+            "../dimacs_benchmark_set/abhik1505040_max_clique_implementations",
+            "anna.col",
+        ),
+    )
+
+    root_parser.add_argument(
+        "-o",
+        "--output-prefix",
+        dest="output_prefix",
+        help="Output path for reports.",
+        default="../results",
+    )
+
+    root_parser.add_argument(
+        "--time-limit",
+        dest="time_limit",
+        help="time limit for each graph run(s).",
+        type=int,
+        default=1200,
+    )
+
+    parser = argparse.ArgumentParser(description="run")
+    subparsers = parser.add_subparsers(dest="method", help="Available methods")
+    subparsers.required = True
+
+    # options for ant-clique
+    parser_aco = subparsers.add_parser(
+        "aco", help="Use ant-clique algorithm", parents=[root_parser]
+    )
+    parser_aco.add_argument(
+        "--ants", dest="num_ants", help="num_ants", type=int, default=7
+    )
+    parser_aco.add_argument(
+        "--taomin", dest="taomin", help="taomin", type=float, default=0.01
+    )
+    parser_aco.add_argument(
+        "--taomax", dest="taomax", help="taomax", type=float, default=4
+    )
+    parser_aco.add_argument(
+        "--alpha", dest="alpha", help="alpha", type=float, default=2
+    )
+    parser_aco.add_argument("--rho", dest="rho", help="rho", type=float, default=0.995)
+    parser_aco.add_argument(
+        "--max_cycles", dest="max_cycles", help="max_cycles", type=int, default=1000
+    )
+    parser_aco.add_argument(
+        "--runs_per_graph",
+        dest="runs_per_graph",
+        help="runs_per_graph",
+        type=int,
+        default=3,
+    )
+
+    # options for bnb
+    parser_bnb = subparsers.add_parser(
+        "bnb", help="Use Branch and Bound algorithm", parents=[root_parser]
+    )
+    parser_bnb.add_argument("--lb", dest="lb", help="lower_bound", type=int, default=0)
+
+    # options for backtracking
+    parser_backt = subparsers.add_parser(
+        "backt", help="Use backtracking algorithm", parents=[root_parser]
+    )
+    parser_backt.add_argument("--graph", dest="graph", help="test_graph")
+
+    return parser.parse_args()
+
+
+def main(args):
+    files, results = [], []
+    if args.input_dir:
+        root_dirs = glob.glob("../dimacs_benchmark_set" + "/*")
+        for dir in root_dirs:
+            for file in os.listdir(dir):
+                files.append(f"{dir}/{file}")
+    elif args.input_path:
+        files = [args.input_path]
+    
+    output_path = f"{args.output_path}/{args.method}_{len(files)}_graphs.csv"
+
+    if args.method == "aco":
+        obj = AntClique(
+            args.num_ants,
+            args.taomin,
+            args.taomax,
+            args.alpha,
+            args.rho,
+            args.max_cycles,
+        )
+
+        for f in files:
+            graph = read_graph(f)
+            outputs = []
+            for i in range(args.runs_per_graph):
+                logger.info(f"Run {i}")
+                try:
+                    with time_limit(args.time_limit):
+                        outputs.append(obj.run(graph))
+                except TimeoutException:
+                    logger.info("Execution timed out!")
+                    continue
+
+            sizes = [o[0] for o in outputs]
+            times = [o[1] for o in outputs]
+            cycles = [o[2] for o in outputs]
+
+            out_json = {
+                "filename": [f],
+                "size->mean(stdev)": [f"{np.mean(sizes):.4f}({np.std(sizes):.4f})"],
+                "time->mean(stdev)": [f"{np.mean(times):.4f}({np.std(times):.4f})"],
+                "cycles->mean(stdev)": [f"{np.mean(cycles):.4f}({np.std(cycles):.4f})"],
+            }
+            log_msg = "Final results-> " + ", ".join(
+                f"{k}: {v[0]}" for k, v in out_json.items()
+            )
+
+            results.append(pd.DataFrame(out_json))
+            logger.info(log_msg)
+            print("\n")
+
+
+    combined_results = pd.concat(results)
+    combined_results.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    args = parse_input_args()
+    main(args)
